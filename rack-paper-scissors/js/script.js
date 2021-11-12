42034e8c7bfb8e1920408d334dec923b3d7a5e9d
0131ryu <89256977+0131ryu@users.noreply.github.com>
2021-11-12 20:56:52 +0900

    JS 기초(for while 등)
    Number() 등 다시 익힘

---
+++ rack-paper-scissors/js/script.js
@@ -0,0 +1,73 @@
+/*
+1. 사용자와 컴퓨터는 동시에 패를 낸다(가위, 바위, 보 중 하나)
+2. 컴퓨터는 매번 랜덤한 패를 낸다
+3. 서로의 패를 비교해서 승자를 정한다
+4. 결과를 알려준다
+*/
+
+//가위, 바위, 보는 계속 반복 -> 상수로 치환(값이 변하지 않음)
+var SCISSORS = "가위";
+var ROCK = "바위";
+var PAPER = "보";
+
+
+var userInput = prompt("가위, 바위, 보!");
+
+//가위, 바위, 보 이외의 값을 입력 시 알림 설정
+if(userInput !== SCISSORS && userInput !== ROCK && userInput !== PAPER) {
+    alert("가위, 바위, 보 중 하나를 입력해야 합니다.");
+} else {
+var comInput;
+
+//Math.random()은 0 ~ 1 사이의 임의의 값 리턴
+var rnd = Math.random();
+
+if(rnd < 0.33) { //0에서 0.33 사이의 값, 즉 1/3 확률
+    comInput = SCISSORS;
+} else if (rnd < 0.66) {
+    comInput = ROCK;
+} else {
+    comInput = PAPER;
+}
+}
+
+//결과 출력하기
+
+
+if(userInput === SCISSORS) {
+    if(comInput === SCISSORS) {
+        //비긴 경우
+        alert("컴퓨터 : " + comInput + " → 컴퓨터와 비겼습니다.");
+    } else if(comInput === ROCK) {
+        //사용자가 진 경우
+        alert("컴퓨터 : " + comInput + " → 컴퓨터에게 졌습니다.");
+    } else {
+         //사용자가 이긴 경우
+         alert("컴퓨터 : " + comInput + " → 컴퓨터에게 이겼습니다.");
+     }
+}  else if(userInput === ROCK) {
+    if(comInput === ROCK) {
+        //비긴 경우
+        alert("컴퓨터 : " + comInput + " → 컴퓨터와 비겼습니다.");
+    } else if(comInput === PAPER) {
+        //사용자가 진 경우
+        alert("컴퓨터 : " + comInput + " → 컴퓨터에게 졌습니다.");
+    } else {
+         //사용자가 이긴 경우
+         alert("컴퓨터 : " + comInput + " → 컴퓨터에게 이겼습니다.");
+     }
+} else {
+    if(comInput === PAPER) {
+        //비긴 경우
+        alert("컴퓨터 : " + comInput + " → 컴퓨터와 비겼습니다.");
+    } else if(comInput === SCISSORS) {
+        //사용자가 진 경우
+        alert("컴퓨터 : " + comInput + " → 컴퓨터에게 졌습니다.");
+    } else {
+         //사용자가 이긴 경우
+         alert("컴퓨터 : " + comInput + " → 컴퓨터에게 이겼습니다.");
+     }
+}
+
+
+console.log("컴퓨터의 패 : ", comInput);
