5da1e86a2fd975216356930555716dcb31a8de5b
0131ryu <89256977+0131ryu@users.noreply.github.com>
2021-11-22 22:56:58 +0900

    JS datepicker를 이용한 날짜 선택 추가
    jquery의 datepicker이용 
    시작날짜와 종료날짜 설정, 종료날짜가 시작날짜보다 우선 되지 않도록 코드 수정

--- my-trip-advisor/js/script.js
+++ my-trip-advisor/js/script.js
@@ -7,5 +7,27 @@ $(function() {
             $('#header').addClass('inverted');
         else
             $('#header').removeClass('inverted');
-    })
-})
+    });
+    $(window).trigger('scroll');
+
+    //Datapicker 사용 -> 오늘 날짜로 표시
+    var dpFrom = $('#from').datepicker({
+        dateFormat: 'yy-mm-dd', //연도-월-일로 표시
+        //입력 가능한 날짜 제한
+        minDate: 0,
+        //시작날짜가 바뀔 경우 종료 날짜의 minDate를 바꿈, 시작날짜 이전으로 종료날짜 변경 안 됨
+        onSelect: function() {
+               dpTo.datepicker('option', 'minDate', dpFrom.datepicker('getDate'));
+        }
+    });
+    dpFrom.datepicker('setDate', new Date());
+
+
+    //종료날짜
+    var dpTo = $('#to').datepicker({
+        dateFormat: 'yy-mm-dd',
+        minDate: 0
+
+    });
+    dpTo.datepicker('setDate', 4)
+});
