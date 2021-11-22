dffc2bfd87a7c36b03e54744fcf330649188289d
0131ryu <89256977+0131ryu@users.noreply.github.com>
2021-11-22 23:19:36 +0900

    JS 검색 시 url 내 정보 나오게 하기
    url과 연결하여 검색 버튼을 누를 경우 정보들이 하단에 나오게 함

--- my-trip-advisor/js/script.js
+++ my-trip-advisor/js/script.js
@@ -30,4 +30,54 @@ $(function() {
 
     });
     dpTo.datepicker('setDate', 4)
+
+    //검색 폼을 제출할 경우 검색함수 호출(button에 submit 존재)
+    $('#form-search').submit(function(e) {
+        e.preventDefault();
+
+        var from = $('#from').val();
+        var to = $('#to').val();
+
+        search(from, to);
+    });
 });
+
+//검색 기능
+function search(from, to) {
+    var url = 'https://javascript-basic.appspot.com/searchLocation';
+
+    $.getJSON(url, {
+        from: from,
+        to: to
+    }, function(r) {
+        //목록함수 추가
+         var $list = $('#list-panel');
+
+            for (var i = 0; i < r.length; i++) {
+              var data = r[i];
+
+              var $item = createListItem(data);
+
+              $list.append($item);
+            }
+
+            $('#list-bg').show();
+          });
+}
+
+//Ajax의 목록함수
+function createListItem(data) {
+  var $tmpl = $('#list-item-template').clone().removeAttr('id');
+
+  $tmpl.find('.list-item-image').attr('src', data.titleImageUrl);
+  $tmpl.find('.list-item-name').html(data.name);
+  $tmpl.find('.list-item-city-name').html(data.cityName);
+
+  $tmpl.click(function(e) {
+    var url = 'detail.html?id=' + data.id;
+
+    window.location = url;
+  });
+
+  return $tmpl;
+}
